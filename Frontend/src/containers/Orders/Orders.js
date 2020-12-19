9f8611f10150350e50a2bcbcfb1ecc230057e295
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2020-12-19 12:24:59 +0100

    Finshed section 12

--- Frontend/src/containers/Orders/Orders.js
+++ Frontend/src/containers/Orders/Orders.js
@@ -1,16 +1,44 @@
+import axios from '../../axios-orders';
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+import WithErrorHandler from './../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+	state = {
+		orders: [],
+		loading: true
+	}
+	componentDidMount(){
+		axios.get('/orders.json')
+			.then(response => {
+				const fetchOrders = [];
+				for(let key in response.data){
+					fetchOrders.push({
+						...response.data[key],
+						Id: key
+					});
+				}
+
+				this.setState({ loading: false, orders: fetchOrders });
+			})
+			.catch(error => {
+				this.setState({ loading: false, });
+			});
+	}
+
 	render (){
 		return (
 			<Auxiliary>
-				<Order></Order>
-				<Order></Order> 
+				{this.state.orders.map(order => (
+					<Order 
+						key={order.id}
+						ingredients={order.ingredients}
+						price={order.price} />
+				))}
 			</Auxiliary>
 		);
 	}
 }
 
-export default Orders;
+export default WithErrorHandler(Orders, axios);
